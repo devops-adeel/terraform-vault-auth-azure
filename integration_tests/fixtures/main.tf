85992028b07990f5c3611ca329bed2b0bf991df4
Adeel Ahmad <adeel@hashicorp.com>
2021-05-26 17:25:36 +0100

    feat: added integration test

--- integration_tests/fixtures/main.tf
+++ integration_tests/fixtures/main.tf
@@ -4,76 +4,19 @@ locals {
   env              = "dev"
   service          = "db"
   application      = "oidc"
+  ad_group         = "aaaatest1"
   mount_accessor   = data.vault_auth_backend.default.accessor
 }
-
-data "azuread_client_config" "default" {}
 
 data "vault_auth_backend" "default" {
   path = "oidc"
 }
 
-resource "azuread_application" "default" {
-  display_name = local.application_name
-  owners       = [data.azuread_client_config.default.object_id]
-
-  api {
-    oauth2_permission_scope {
-      admin_consent_description  = "Allow the HCP-Vault to validate AD user logging into Vault."
-      admin_consent_display_name = "HCP Vault"
-      enabled                    = true
-      id                         = "00000003-0000-0000-c000-000000000000"
-      type                       = "User"
-      value                      = "GroupMember.Read.All"
-    }
-    oauth2_permission_scope {
-      admin_consent_description  = "Allow the application to access AD on behalf of the signed-in user."
-      admin_consent_display_name = "Access Impersonation"
-      enabled                    = true
-      id                         = "96183846-204b-4b43-82e1-5d2222eb4b9b"
-      type                       = "User"
-      user_consent_description   = "Allow the application to access example on your behalf."
-      user_consent_display_name  = "Access example"
-      value                      = "user_impersonation"
-    }
-  }
-  web {
-    redirect_uris = ["http://localhost:8250/oidc/callback"]
-
-    implicit_grant {
-      access_token_issuance_enabled = true
-    }
-  }
-}
-
-resource "azuread_service_principal" "default" {
-  application_id               = azuread_application.default.application_id
-  app_role_assignment_required = false
-}
-
-resource "vault_jwt_auth_backend_role" "default" {
-  backend               = data.vault_auth_backend.default.path
-  role_name             = "default"
-  token_policies        = ["default"]
-  user_claim            = "email"
-  groups_claim          = "groups"
-  role_type             = "oidc"
-  allowed_redirect_uris = ["http://localhost:8200/ui/vault/auth/oidc/oidc/callback"]
-  oidc_scopes           = ["https://graph.microsoft.com/.default"]
-}
-
-resource "vault_identity_group" "default" {
-  name = local.role_id
-  type = "external"
-  metadata = {
-    env         = local.env
-    service     = local.service
-    application = local.application
-  }
-}
-
-resource "vault_identity_group_alias" "default" {
-  name           = "aaaatest1"
-  mount_accessor = local.mount_accessor
-  canonical_id   = vault_identity_group.default.id
+module "default" {
+  source           = "./module"
+  ad_group         = local.ad_group
+  application_name = local.application_name
+  env              = local.env
+  service          = local.service
+  mount_accessor   = local.mount_accessor
 }
--- main.tf
+++ main.tf
@@ -16,8 +16,7 @@
 
 
 locals {
-  role_id        = format("%s-%s", var.application_name, var.service)
-  group_id       = var.identity_group_id
+  ad_group       = var.ad_group
   env            = var.env
   service        = var.service
   application    = var.application_name
@@ -27,16 +26,16 @@ locals {
 
 resource "vault_jwt_auth_backend_role" "default" {
   backend               = local.backend_path
-  role_name             = local.role_id
+  role_name             = local.ad_group
   role_type             = local.backend_path
   user_claim            = "email"
   groups_claim          = "groups"
   allowed_redirect_uris = ["http://localhost:8200/ui/vault/auth/oidc/oidc/callback"]
-  oidc_scopes           = "https://graph.microsoft.com/.default"
+  oidc_scopes           = ["https://graph.microsoft.com/.default"]
 }
 
 resource "vault_identity_group" "default" {
-  name = local.role_id
+  name = local.ad_group
   type = "external"
   metadata = {
     env         = local.env
@@ -46,13 +45,32 @@ resource "vault_identity_group" "default" {
 }
 
 resource "vault_identity_group_alias" "default" {
-  name           = "OIDC_AD_Group_Slug"
+  name           = local.ad_group
   mount_accessor = local.mount_accessor
   canonical_id   = vault_identity_group.default.id
 }
 
-resource "vault_identity_group_member_group_ids" "default" {
-  member_entity_ids = [vault_identity_group.default.id]
-  exclusive         = false
-  group_id          = local.group_id
+data "vault_policy_document" "default" {
+  rule {
+    path         = "secret/+/{{identity.groups.ids.${vault_identity_group.default.id}.metadata.env}}-{{identity.groups.ids.${vault_identity_group.default.id}.metadata.service}}/*"
+    capabilities = ["create", "read", "update", "delete", "list"]
+    description  = "allow read of static secret object named after metadata keys"
+  }
+  rule {
+    path         = "auth/token/*"
+    capabilities = ["create", "read", "update", "delete", "list"]
+    description  = "create child tokens"
+  }
+}
+
+resource "vault_policy" "default" {
+  name   = "${local.ad_group}-default-kv-store"
+  policy = data.vault_policy_document.default.hcl
+}
+
+
+resource "vault_identity_group_policies" "default" {
+  group_id  = vault_identity_group.default.id
+  exclusive = false
+  policies  = [vault_policy.default.name]
 }
